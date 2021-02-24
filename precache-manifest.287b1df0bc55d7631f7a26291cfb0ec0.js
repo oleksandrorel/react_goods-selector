a512f0f27106f8d65441e3f4579344e0e3c39caa
Oleksandr Orel <oleksandrorel@ukr.net>
2021-02-24 17:58:33 +0200

    Updates

---
+++ precache-manifest.287b1df0bc55d7631f7a26291cfb0ec0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628639bbec3195f05f0b423f59fbad20",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f7c0ab75fb6ebdf9a6d3",
+    "url": "./static/css/main.3fab2a43.chunk.css"
+  },
+  {
+    "revision": "00f97512371894b1ef1f",
+    "url": "./static/js/2.cd5b928f.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.cd5b928f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7c0ab75fb6ebdf9a6d3",
+    "url": "./static/js/main.093ae42d.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
